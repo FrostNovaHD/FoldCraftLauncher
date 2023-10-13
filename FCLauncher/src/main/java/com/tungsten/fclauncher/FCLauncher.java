f422be1093089a13d9c30349041476561c81cdaa
LegacyGamerHD <wesleyvanneck1012@gmail.com>
2023-10-13 17:45:22 +0200

    more cleanup

--- FCLauncher/src/main/java/com/tungsten/fclauncher/FCLauncher.java
+++ FCLauncher/src/main/java/com/tungsten/fclauncher/FCLauncher.java
@@ -144,23 +144,18 @@ public class FCLauncher {
             envMap.put("LIBGL_ES","3");
         } else {
             envMap.put("MESA_GLSL_CACHE_DIR", config.getContext().getCacheDir().getAbsolutePath());
-            envMap.put("MESA_GL_VERSION_OVERRIDE", renderer == FCLConfig.Renderer.RENDERER_VIRGL ? "4.0" : "4.6");
-            envMap.put("MESA_GLSL_VERSION_OVERRIDE", renderer == FCLConfig.Renderer.RENDERER_VIRGL ? "400" : "460");
+            envMap.put("MESA_GL_VERSION_OVERRIDE", renderer == FCLConfig.Renderer.RENDERER_VIRGL ? "3.3" : "4.6");
+            envMap.put("MESA_GLSL_VERSION_OVERRIDE", renderer == FCLConfig.Renderer.RENDERER_VIRGL ? "330" : "460");
             envMap.put("force_glsl_extensions_warn", "true");
             envMap.put("allow_higher_compat_version", "true");
             envMap.put("allow_glsl_extension_directive_midshader", "true");
             envMap.put("MESA_LOADER_DRIVER_OVERRIDE", "zink");
-	    envMap.put("ZINK_DESCRIPTORS", "lazy");
             envMap.put("VTEST_SOCKET_NAME", new File(config.getContext().getCacheDir().getAbsolutePath(), ".virgl_test").getAbsolutePath());
         if (renderer == FCLConfig.Renderer.RENDERER_VIRGL) {
             envMap.put("GALLIUM_DRIVER", "virpipe");
-	    envMap.put("LIBGL_ES","3");
             envMap.put("OSMESA_NO_FLUSH_FRONTBUFFER", "1");
        } else if (renderer == FCLConfig.Renderer.RENDERER_ZINK) {
             envMap.put("GALLIUM_DRIVER", "zink");
-	    envMap.put("MESA_LOADER_DRIVER_OVERRIDE", "zink");
-            envMap.put("ZINK_DESCRIPTORS", "lazy");
-	    envMap.put("OSMESA_NO_FLUSH_FRONTBUFFER", "1");
 
 
             }
